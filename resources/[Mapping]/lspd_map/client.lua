7bb584fe0aed616e1477dfe140ac1ffefd8018cd
Five-Dev <haxy60@gmail.com>
2021-04-05 12:14:25 +0200

    Push Mapping

---
+++ resources/[Mapping]/lspd_map/client.lua
@@ -0,0 +1,3 @@
+local Interior = GetInteriorAtCoords(440.84, -983.14, 30.69)
+
+LoadInterior(Interior)
---
+++ resources/[Mapping]/lspd_map/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "adamant"
+
+game "gta5"
+
+this_is_a_map 'yes'
+
+client_scripts {
+    "client.lua",
+}
